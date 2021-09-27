083d62c0a7079fd1e33378266b7e3a1798092bcd
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-09-27 17:02:49 +0300

    addbrowser

--- app/src/main/java/com/example/browser/MainActivity.java
+++ app/src/main/java/com/example/browser/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.browser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +17,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WebView myWebView = (WebView) findViewById(R.id.htmlContent);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://google.com");
 
+        EditText keywords = findViewById(R.id.keywords);
+        ImageButton searchBtn = findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.loadUrl(keywords.getText().toString());
+            }
+        });
+
+        ImageButton burgerBtn = findViewById(R.id.burgerBtn);
+        burgerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.loadUrl(keywords.getText().toString());
+            }
+        });
+
+        ImageButton tabsBtn = findViewById(R.id.tabsBtn);
+        tabsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.loadUrl(keywords.getText().toString());
+            }
+        });
+
+        ImageButton bookmarkBtn = findViewById(R.id.bookmarkBtn);
+        bookmarkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.loadUrl(keywords.getText().toString());
+            }
+        });
+
+        ImageButton homeBtn = findViewById(R.id.homeBtn);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.loadUrl("https://google.com");
+            }
+        });
+
+        ImageButton rightArrowBtn = findViewById(R.id.rightArrowBtn);
+        rightArrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.goForward();
+            }
+        });
+
+        ImageButton leftArrowBtn = findViewById(R.id.leftArrowBtn);
+        leftArrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myWebView.goBack();
+            }
+        });
 
     }
 }
